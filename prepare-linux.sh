06ac4f85020305b193714d7af03a05811721dda5
Levi Mota <levi.mota@gmail.com>
2024-12-24 12:00:00 -0300

    Add files via upload

---
+++ prepare-linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+
+# Instalar apt-fast
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt update
+sudo apt -y install apt-fast
+
+# Atualizar o sistema (agora em paralelo)
+sudo apt-fast update
+sudo apt-fast upgrade
+
+# Instalar Ubuntu Restricted Extras (https://en.wikipedia.org/wiki/Ubuntu-restricted-extras) 
+sudo apt-fast -y install ubuntu-restricted-extras
+
+# Instalar NeoFetch
+sudo apt-fast -y install neofetch
+
+# Instalar Gerenciador de pacotes Synaptic
+sudo apt-fast -y install synaptic
+
