be70a6980e78e0193f34c29eb2764147f5ad95fb
Levi Mota <levi.mota@gmail.com>
2025-01-07 22:12:02 -0300

    Update

--- ubuntu-scripts/install-essential-apps.sh
+++ ubuntu-scripts/install-essential-apps.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 cd ~/Downloads
 
-sudo apt update
-
-echo Installing apt-fast..
-sudo add-apt-repository ppa:apt-fast/stable
-sudo apt update
-sudo apt -y install apt-fast
-
-echo Updating system \(now in parallel\)...
-sudo apt update
-sudo apt-fast upgrade
-
-echo Installing Ubuntu Restricted Extras \(https://en.wikipedia.org/wiki/Ubuntu-restricted-extras\)...
-sudo apt-fast -y install ubuntu-restricted-extras
-
 echo Installing Synaptic...
 sudo apt-fast -y install synaptic
 
--- ubuntu-scripts/prepare-linux.sh
+++ ubuntu-scripts/prepare-linux.sh
@@ -3,6 +3,20 @@ echo Setting date do local time, like Windows does...
 timedatectl set-local-rtc 1 --adjust-system-clock
 timedatectl
 
+sudo apt update
+
+echo Installing apt-fast..
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt update
+sudo apt -y install apt-fast
+
+echo Updating system \(now in parallel\)...
+sudo apt update
+sudo apt-fast upgrade
+
+echo Installing Ubuntu Restricted Extras \(https://en.wikipedia.org/wiki/Ubuntu-restricted-extras\)...
+sudo apt-fast -y install ubuntu-restricted-extras
+
 ./install-essential-apps.sh
 
 ./install-dev-tools.sh
