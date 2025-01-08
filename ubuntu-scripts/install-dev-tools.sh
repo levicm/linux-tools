62b13a4e63a51c26f0af9867ea1e28ae54787cd2
Levi Mota <levi.mota@gmail.com>
2025-01-07 21:25:47 -0300

    Installing Postman

--- ubuntu-scripts/install-dev-tools.sh
+++ ubuntu-scripts/install-dev-tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~/Downloads
-# Install dev tools
+
 echo Installing Node.js v20 and Npm...
 # sudo apt-fast -y install nodejs npm
-# This installation gets just old version 12
+# The apt installation gets just old version 12
 # Using fnm we can get more recent versions, as v20...
 curl -fsSL https://fnm.vercel.app/install | bash
 source /home/levi/.bashrc
@@ -13,6 +13,7 @@ echo Installing Angular 17...
 npm install -g @angular/cli@17
 
 echo Installing VSCode...
+cd ~/Downloads
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
@@ -20,6 +21,7 @@ sudo apt update
 sudo apt-fast install code
 
 echo Installing Eclipse IDE...
+cd ~/Downloads
 wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2024-12/R/eclipse-jee-2024-12-R-linux-gtk-x86_64.tar.gz
 cd /opt
 sudo tar -xvzf ~/Downloads/eclipse-jee-*
@@ -38,3 +40,20 @@ Name[en]=Eclipse
 Name[en_US]=Eclipse
 EOF
 sudo desktop-file-install eclipse.desktop
+
+echo Installing Postman...
+cd ~/Downloads
+wget wget --output-document=postman.tar.gz https://dl.pstmn.io/download/latest/linux_64
+tar -xf postman.tar.gz
+sudo mv ./Postman /opt
+cat > postman.desktop <<EOF
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=/opt/Postman/app/Postman %U
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOF
+sudo desktop-file-install postman.desktop
