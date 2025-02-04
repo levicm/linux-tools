44e50e43c67b3ee7a69121cce46ca1f9505c7b72
Levi Mota <levi.mota@gmail.com>
2025-02-04 14:27:23 -0300

    Update install-dev-tools.sh

--- ubuntu-scripts/install-dev-tools.sh
+++ ubuntu-scripts/install-dev-tools.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-echo Installing GIT...
-sudo apt -y install git
-
-echo Installing Python3 and pip...
-sudo apt -y install python3
-sudo apt -y install python3-pip
-
 ./develompent/install-vscode.sh
 
 ./develompent/install-eclipse.sh
