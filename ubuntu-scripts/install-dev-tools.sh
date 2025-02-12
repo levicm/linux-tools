e9be79924ca6b8b56ed21bb092918cdcd15adc16
Levi Mota <levi.mota@gmail.com>
2025-02-11 21:56:41 -0300

    Update install-dev-tools.sh

--- ubuntu-scripts/install-dev-tools.sh
+++ ubuntu-scripts/install-dev-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-bash <(wget -qO - https://raw.githubusercontent.com/levicm/linux-tools/refs/heads/main/ubuntu-scripts/develompent/install-vscode.sh)
+bash <(wget -qO - https://raw.githubusercontent.com/levicm/linux-tools/refs/heads/main/ubuntu-scripts/development/install-vscode.sh)
 
-bash <(wget -qO - https://raw.githubusercontent.com/levicm/linux-tools/refs/heads/main/ubuntu-scripts/develompent/install-eclipse.sh)
+bash <(wget -qO - https://raw.githubusercontent.com/levicm/linux-tools/refs/heads/main/ubuntu-scripts/development/install-eclipse.sh)
 
-bash <(wget -qO - https://raw.githubusercontent.com/levicm/linux-tools/refs/heads/main/ubuntu-scripts/develompent/install-postman.sh)
+bash <(wget -qO - https://raw.githubusercontent.com/levicm/linux-tools/refs/heads/main/ubuntu-scripts/development/install-postman.sh)
