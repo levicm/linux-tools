3cc3ff1245ca4923201f5f8034848952beee7982
Levi Mota <levi.mota@gmail.com>
2025-04-20 15:58:46 -0300

    Refactor DBeaver installation script to use repository instead of local deb package

--- ubuntu-scripts/development/install-debeaver.sh
+++ ubuntu-scripts/development/install-debeaver.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo Installing DBeaver...
-cd ~/Downloads
-wget -c https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-sudo dpkg -i dbeaver-ce_latest_amd64.deb
-cd ~/
+# Using debian package...
+#cd ~/Downloads
+#wget -c https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+#sudo dpkg -i dbeaver-ce_latest_amd64.deb
+#cd ~/
+
+# Using repository...
+sudo wget -O /usr/share/keyrings/dbeaver.gpg.key https://dbeaver.io/debs/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update 
+sudo apt-get install dbeaver-ce
