dd535901c53ebd5c96d4bb87267c3c8448d47fd7
Levi Mota <levi.mota@gmail.com>
2025-06-20 12:35:20 -0300

    Update Maven installation script to version 3.9.10

--- ubuntu-scripts/development/install-maven-3.sh
+++ ubuntu-scripts/development/install-maven-3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo Installing Maven 3.9.9...
+echo Installing Maven 3.9.10...
 cd ~/Downloads
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.zip
-unzip apache-maven-3.9.9-bin.zip
-sudo mv apache-maven-3.9.9 /opt/
-sudo update-alternatives --install /usr/bin/mvn maven /opt/apache-maven-3.9.9/bin/mvn 399
+wget https://dlcdn.apache.org/maven/maven-3/3.9.10/binaries/apache-maven-3.9.10-bin.zip
+unzip apache-maven-3.9.10-bin.zip
+sudo mv apache-maven-3.9.10 /opt/
+sudo update-alternatives --install /usr/bin/mvn maven /opt/apache-maven-3.9.10/bin/mvn 3910
 mvn -v
