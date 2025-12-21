f22ce366664ab91b6acbadf81d4dc52d7788dffa
Levi Mota <levi.mota@gmail.com>
2025-12-21 19:20:18 -0300

    Update Bitwarden installation script to use Flatpak and comment out .deb installation

--- ubuntu-scripts/security/install-bitwarden.sh
+++ ubuntu-scripts/security/install-bitwarden.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo Installing Bitwarden...
-cd ~/Downloads
-wget -c https://github.com/bitwarden/clients/releases/download/desktop-v2024.12.1/Bitwarden-2024.12.1-amd64.deb
-sudo apt -y install ./Bitwarden-2024.12.1-amd64.deb
+
+# Instala pacote .deb oficial, mas não atualiza automaticamente
+#cd ~/Downloads
+#wget -c https://github.com/bitwarden/clients/releases/download/desktop-v2024.12.1/Bitwarden-2024.12.1-amd64.deb
+#sudo apt -y install ./Bitwarden-2024.12.1-amd64.deb
+
+# Instala via Flatpak, atualiza automaticamente
+flatpak install flathub com.bitwarden.desktop -y
