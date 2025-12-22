be4e6207aab17f0bb4eb71a29c2152f395dbba0f
Levi Mota <levi.mota@gmail.com>
2025-12-22 13:36:05 -0300

    Enhance Docker installation script to configure Docker without sudo and install Docker Hub certificate

--- ubuntu-scripts/development/install-docker.sh
+++ ubuntu-scripts/development/install-docker.sh
@@ -12,3 +12,19 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo Configuring Docker to run without sudo...
+sudo usermod -aG docker $USER
+newgrp docker
+
+
+# Docker Hub certificate
+echo Installing DockerHub certificate...
+openssl s_client -showcerts -connect registry-1.docker.io:443 </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > docker.com.crt
+sudo cp docker.com.crt /usr/local/share/ca-certificates/
+# atualiza os certificados do sistema copiados para /usr/local/share/ca-certificates/
+sudo apt-get install -y ca-certificates
+sudo update-ca-certificates
+# os certificados copiados para /etc/docker/certs.d/ serão utilizados diretamente pelo Docker
+sudo mkdir -p /etc/docker/certs.d/registry-1.docker.io/
+sudo cp docker.com.crt /etc/docker/certs.d/registry-1.docker.io/
