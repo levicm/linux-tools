111e20f6590ecf2187da872b14c7d0a4b9c33bb2
Levi Mota <levi.mota@gmail.com>
2026-01-05 14:38:17 -0300

    Fix installation script for Ookla Speed Test by removing problematic repository cleanup

--- ubuntu-scripts/tool/install-speedtest.sh
+++ ubuntu-scripts/tool/install-speedtest.sh
@@ -5,5 +5,3 @@ echo
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt update
 sudo apt -y install speedtest-cli
-# Remove the repository from the sources list because it is problematic
-sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
