e8ba793c5842d598d49f66b420e3612606aecf19
Levi Mota <levi.mota@gmail.com>
2026-01-07 14:41:11 -0300

    Fix installation script for Ookla Speed Test by correcting package name and adding manual installation fallback

--- ubuntu-scripts/tool/install-speedtest.sh
+++ ubuntu-scripts/tool/install-speedtest.sh
@@ -4,4 +4,14 @@ echo Installing Ookla Speed Test...
 echo
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt update
-sudo apt -y install speedtest-cli
+sudo apt -y install speedtest
+
+if [ $? -ne 0 ]; then 
+    echo "Falhou via apt, instalando manualmente..." 
+    original_dir=$(pwd)
+    cd ~/Downloads
+    wget https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz
+    tar -xvzf ookla-speedtest-1.2.0-linux-x86_64.tgz
+    sudo mv speedtest /usr/local/bin/
+    cd $original_dir
+fi
