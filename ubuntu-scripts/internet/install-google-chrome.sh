ae2727cc83437a7d1d7d64c7a112d9ede9866b27
Levi Mota <levi.mota@gmail.com>
2026-02-15 21:57:01 -0300

    Refactor Google Chrome installation script to use APT repository method for improved dependency management

--- ubuntu-scripts/internet/install-google-chrome.sh
+++ ubuntu-scripts/internet/install-google-chrome.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo Installing Google Chrome...
-cd ~/Downloads
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt -y install ./google-chrome-stable_current_amd64.deb
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-linux-signing-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-linux-signing-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update 
+sudo apt install google-chrome-stable
+# Installing directly from the .deb file since the repository method can sometimes cause issues with dependencies.
+#cd ~/Downloads
+#wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo apt -y install ./google-chrome-stable_current_amd64.deb
