6104852a21993c019fbd7624f4f2b8555b403be3
Levi Mota <levi.mota@gmail.com>
2026-02-24 08:56:35 -0300

    Refactor DBeaver installation script to remove outdated comments and ensure correct key file naming

--- ubuntu-scripts/development/install-debeaver.sh
+++ ubuntu-scripts/development/install-debeaver.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 echo Installing DBeaver...
-# Using debian package...
-#cd ~/Downloads
-#wget -c https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-#sudo dpkg -i dbeaver-ce_latest_amd64.deb
-#cd ~/
-
-# Using repository...
-sudo wget -O /usr/share/keyrings/dbeaver.gpg.key https://dbeaver.io/debs/dbeaver.gpg.key
-echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo wget -O /usr/share/keyrings/dbeaver.gpg https://dbeaver.io/debs/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
 sudo apt-get update 
 sudo apt-get install dbeaver-ce
