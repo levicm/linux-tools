93d9fb07713b1ae65329bfd9469f12a84ba17541
Levi Mota <levi.mota@gmail.com>
2026-02-24 08:57:06 -0300

    Update Spotify installation script to use new GPG key and improve keyring handling

--- ubuntu-scripts/media/install-spotify.sh
+++ ubuntu-scripts/media/install-spotify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Installing Spotify...
-curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+curl -fsSL https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify.gpg
+echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt -y install spotify-client
