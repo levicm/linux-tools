6c475f100a0c0fd8331a4d8e76bdd75f3886fd6c
Levi Mota <levi.mota@gmail.com>
2026-03-11 10:40:46 -0300

    Update DBeaver installation script to improve GPG key handling

--- ubuntu-scripts/development/install-debeaver.sh
+++ ubuntu-scripts/development/install-debeaver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Installing DBeaver...
-sudo wget -O /usr/share/keyrings/dbeaver.gpg https://dbeaver.io/debs/dbeaver.gpg.key
+wget -O- https://dbeaver.io/debs/dbeaver.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/dbeaver.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
 sudo apt-get update 
 sudo apt-get install dbeaver-ce
